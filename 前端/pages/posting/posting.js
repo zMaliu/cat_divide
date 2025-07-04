1497bd6e1dc9a58a42c4916dae84737822502e2b
Research972 <2609859370@qq.com>
2025-07-04 20:18:34 +0800

    发作品

--- 前端/pages/posting/posting.js
+++ 前端/pages/posting/posting.js
@@ -2,24 +2,24 @@ Page({
     data: {
       currentTab: 'posting',
       postingForm: {
-        photo: '',
+        // photo: '',
         title: '',
         content:''
       },
     },
 
-    onChoosePhoto: function(e) {
-        wx.chooseImage({
-            count:1,
-            sizeType: ['original', 'compressed'],
-            sourceType: ['album', 'camera'], 
-            success: (res) => {
-                this.setData({
-                  'postingForm.photo': res.tempFilePaths[0]
-                }); 
-            }  
-        })
-    },
+    // onChoosePhoto: function(e) {
+    //     wx.chooseImage({
+    //         count:1,
+    //         sizeType: ['original', 'compressed'],
+    //         sourceType: ['album', 'camera'], 
+    //         success: (res) => {
+    //             this.setData({
+    //               'postingForm.photo': res.tempFilePaths[0]
+    //             }); 
+    //         }  
+    //     })
+    // },
 
     onPostingTitleInput: function (e) {
         this.setData({
@@ -35,20 +35,23 @@ Page({
     },
 
     handleRegister: function () {
-        const { photo,title,content } = this.data.postingForm;
-        console.log("photo",photo);
+        const { title,content } = this.data.postingForm;  //这一步是定义变量
+        // console.log("photo",photo);
         console.log("title",title);
         console.log("content",content);
         wx.request({
             url: 'http://localhost:5001/api/register',
             method: 'POST',
-            //filePath: imagePath,
-            filePath: this.data.postingForm.image,
+            // filePath: photo,      photo变量本身就是一个路径，这个写法可以，下面这个也可以
+            // filePath: this.data.postingForm.photo,
+            // 对this的理解
+            // this 让你能在页面的任意方法里访问和操作当前页面的数据。只要你在 data 里定义了变量，
+            // 就可以通过this.data.xxx 访问。直接写变量名（如 imagePath）只有在你本函数里定义了才行，否则会报错。
             header: {
             'Content-Type': 'application/json'
             },
             data: {
-                photo: photo,
+                // photo: photo,
                 title: title,
                 content:content
             },
